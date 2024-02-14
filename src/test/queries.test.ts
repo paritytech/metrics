97113560a823c4e3eb8be2f2b4591f22d5935cf7
Javier Bullrich <javier@bullrich.dev>
2024-02-14 15:31:57 +0100

    added query to obtain issues

--- src/test/queries.test.ts
+++ src/test/queries.test.ts
@@ -2,8 +2,14 @@ import { validate } from "@octokit/graphql-schema";
 
 import PULL_REQUEST_LIST_QUERY from "../github/queries/PullRequestList";
 
+import ISSUE_LIST_QUERY from "../github/queries/IssueList";
+
 describe("Validate Schemas", () => {
   test("PULL_REQUEST_LIST_QUERY", () => {
     expect(validate(PULL_REQUEST_LIST_QUERY)).toEqual([]);
+  });
+  
+  test("ISSUE_LIST_QUERY", () => {
+    expect(validate(ISSUE_LIST_QUERY)).toEqual([]);
   });
 });
