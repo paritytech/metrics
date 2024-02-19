33b62ca6cdb36249de540ab9d68d9cbd0b5887e8
Javier Bullrich <javier@bullrich.dev>
2024-02-19 13:13:10 +0100

    generated output in the action file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { getInput, setFailed, setOutput } from "@actions/core";
 import { context, getOctokit } from "@actions/github";
 import { Context } from "@actions/github/lib/context";
+import { writeFile } from "fs/promises";
 
 import { getMetrics } from "./analytics";
 import { generateCoreLogger } from "./util";
@@ -25,5 +26,13 @@ setOutput("repo", `${repo.owner}/${repo.repo}`);
 
 const token = getInput("GITHUB_TOKEN", { required: true });
 getMetrics(getOctokit(token), generateCoreLogger(), repo)
-  .then(async (result) => await result.summary.write())
+  .then(async (result) => {
+    // We write the job output
+    await result.summary.write();
+    await writeFile("./report.md", result.summary.stringify());
+
+    // We set the report for both outputs
+    setOutput("pr-report", JSON.stringify(result.prMetrics));
+    setOutput("issue-report", JSON.stringify(result.issueMetrics));
+  })
   .catch(setFailed);
